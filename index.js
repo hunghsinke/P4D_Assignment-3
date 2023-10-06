fe516e91da18c34f079315b8a6341c59541ecd59
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-10-06 17:26:07 +1100

    update JS and CSS
    Update css font size for trees section.
    
    Add JS for scroll function. The code can make the header disappear when user scrolls down, and reappear when user scroll up and might need the header

--- index.js
+++ index.js
@@ -6,3 +6,42 @@ document.querySelector("#trees").style.color='white'
 addEventListener("scroll", (event) => {
     console.log('ScrollY: '+ scrollY);
 });
+
+// // hide/show header on click
+// let header = document.querySelector("header")
+// header.addEventListener("click", (event) => {
+//   console.log(header)
+//   let op = header.style.opacity
+//   if(op == "1"){
+//     header.style.opacity = "0"
+//   }else{
+//     header.style.opacity = "1"
+//   }
+// });
+
+
+// the below code can make the header disappear when user scrolls down, and reappear when user scroll up and might need the header 
+// global vars
+var lastScroll
+
+// a generic listener, triggers when the page is scrolled
+document.addEventListener("scroll", (event) => {
+  // let's see where we are
+  console.log("scroll y: " + scrollY)
+
+  // and add some logic to make this event useful
+  
+  // if we're scrolling up the page
+  if(scrollY < lastScroll){
+    document.querySelector("header").style.opacity = '1'
+  // or down
+  }else{
+    document.querySelector("header").style.opacity = '0'
+  }
+
+  // a global variable storing the position 
+  // of the scrollbar when scrolling stops
+  lastScroll = scrollY
+});
+
+// ************* end *************

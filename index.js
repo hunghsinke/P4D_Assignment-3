7df45ff8753989253193a0c7c49bfd1e8bc07056
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-10-06 15:42:33 +1100

    Update Logo and Scroll Y
    Add Logo code and Scroll Y values.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 
 // query select id = 'trees', and change the colour to white 
 document.querySelector("#trees").style.color='white'
+
+//shows the ScrollY in console when the user is scrolling 
+addEventListener("scroll", (event) => {
+    console.log('ScrollY: '+ scrollY);
+});

38c6dd1e17f45fc044fc421c4925b5338e4c7619
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-10-13 14:13:18 +1100

    Update glide slideshow
    Update glide slideshow.
    import glide library and the relevant files.
    Modify html css and add JS code to make slides with glide library.

---
+++ JS/testing_slides.js
@@ -0,0 +1,16 @@
+//https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.0.2/glide.js
+
+var glideMulti = new Glide('.multi', {
+    type: 'slider',
+    autoplay: 4000,
+    focusAt: 'center',
+    perView: 2
+  });
+  
+  
+  glideMulti.mount();
+ 
+  
+  
+  
+  

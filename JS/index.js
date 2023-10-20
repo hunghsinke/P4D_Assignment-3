dd5e767be2341a394639bff4c3f0cbaeae4a07fc
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-10-20 21:41:53 +1100

    Update video and layout of the website
    Update video section and layout of the website.
    
    Design three columns in a section.

--- JS/index.js
+++ JS/index.js
@@ -95,3 +95,24 @@ solarButton.onclick = () =>{
   }
 };
 
+// JS Masonry Grid 
+
+// var elem = document.querySelector('.grid');
+// var msnry = new Masonry( elem, {
+//   // options
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var msnry = new Masonry( '.grid', {
+//   // options
+// });
+
+
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});

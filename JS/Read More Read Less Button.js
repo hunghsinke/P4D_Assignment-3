69c92a90971e4d07ebfecc0febeb3f14d609140a
Hung Hsin (Kenny)Ke <78602893+hunghsinke@users.noreply.github.com>
2023-10-27 16:17:09 +1100

    update jquery for stone section
    Add jquery so that everytime the user click on Read More, it expands the text.
    This allow us to not write three functions to perform the same action.
    If we need to write three functions we need to have different IDs assigned to each card.
    Now I need to figure out how to update the word 'Read More' to 'Read less'

---
+++ JS/Read More Read Less Button.js
@@ -0,0 +1,39 @@
+function myFunction() {
+  var dots = document.getElementsById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+// another way to write this 
+let buttons = document.querySelectorAll(".myBtn")
+console.log(buttons)
+for (i = 0; i< buttons.length; i++){
+  buttons[i].addEventListener('click',(event)=>{
+    let dots = event.currentTarget
+    let siblings =   $(dots).siblings(".more")
+
+    console.log($(dots).siblings(".more"))
+  
+
+    if ( siblings.css("display") === "none") {
+      siblings.css("display", "inline");
+      // btnText.innerHTML = "Read more"; 
+      // moreText.style.display = "none";
+    } else {
+      siblings.css("display", "none");
+      // btnText.innerHTML = "Read less"; 
+      // moreText.style.display = "inline";
+    }
+
+  })
+}
